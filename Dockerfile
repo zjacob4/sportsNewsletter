de301d9f3cc6cf4055a0a0cf01b789c8969929c3
Zach Jacob <zachjacob96@gmail.com>
2025-01-20 22:58:17 -0500

    update main py with latest

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["gunicorn", "-b", "0.0.0.0:8080", "--timeout", "120", "main:app"]
+CMD ["gunicorn", "-b", "0.0.0.0:8080", "--timeout", "240", "main:app"]
--- main.py
+++ main.py
@@ -1,28 +1,32 @@
-import logging
 from flask import Flask, render_template
 from news_team import write_stories
 from create_stylesheet import write_css_stylesheet
+from graphics_team import add_graphics
+from research_team import research_stories
+from fact_check_team import fact_check_stories
 
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.DEBUG)
-
 @app.route('/')
 def index():
-    try:
-        # Fetch the daily news headlines from an API or a database
-        headlines = [
-            # Add more headlines here
-        ]
-        
-        # Generate or fetch the result
-        result = write_stories()
-        write_css_stylesheet(result)
+    # Fetch the daily news headlines from an API or a database
+    headlines = [
+        # Add more headlines here
+    ]
+    
+    # Generate or fetch the result
+    stories = research_stories()
+    stories = fact_check_stories(stories)
+    with open('latest_text/latest_research_fact_check.txt', 'w') as file:
+        file.write(stories)
+    result = write_stories(stories)
+    result = fact_check_stories(result)
+    with open('latest_text/latest_writing_fact_check.txt', 'w') as file:
+        file.write(result)
+    result = add_graphics(result)
+    write_css_stylesheet(result)
 
-        return render_template('index.html', headlines=headlines, result=result)
-    except Exception as e:
-        logging.error(f"Error in index route: {e}")
-        return "An error occurred", 500
+    return render_template('index.html', headlines=headlines, result=result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8082)
